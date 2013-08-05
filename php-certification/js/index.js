3278002e2ee67e4c81d02af2648ce22f0c83797c
bernatmv <Pollux3435>
2013-08-05 15:06:44 +0200

    Testing

--- php-certification/js/index.js
+++ php-certification/js/index.js
@@ -1,14 +1,14 @@
+// config data
+var BASE_URL = '';
+var OFFLINE_MODE = false;
 
 // jqTouch
-
 $.jQTouch({
-    icon: 'jqtouch.png',
     statusBar: 'black-translucent',
     preloadImages: []
 });
 
 // app Object
-
 var app = {
 
     // Application Constructor
@@ -24,10 +24,43 @@ var app = {
 
     onDeviceReady: function() {
 
-        jQT.addAnimation({
-            name: "reveal",
-            selector: ".revealme"
-        });
-    }
+        // write loading, show and load question
+        $('.question-token').on('click', function (e) {
+            // start loading and show
+            app.setQuestiontitle('Loading...');
+            $('#question .loading').show();
 
+            var questionNumber = this.getAttribute('data-question-number');
+            // show question
+            if (OFFLINE_MODE) {
+                $('#question .loading').hide();
+
+            }
+            else {
+                $.ajax({
+                    url: BASE_URL + '/questions/question_' + questionNumber + '.html',
+                    dataType: "html",
+                    timeoute: 10000,
+                    success: function (data, status, xhr) {
+                        $('#question .loading').hide();
+
+                    },
+                    error: function (xhr, errorType, error) {
+                        $('#question .loading').hide();
+
+                    }
+                });
+            }
+        });
+
+        // hide question and re-write loading
+        $('.question-out').on('click', function (e) {
+            app.setQuestiontitle('Loading...');
+            $('#question .loading').show();
+        });
+    },
+
+    setQuestiontitle: function(title) {
+        $('#question .toolbar h1').html(title);
+    }
 };
